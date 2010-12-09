59dcf56cea16cbe0a7cda09d3f8e651b179a03b7
andrey <a.mandyev@gmail.com>
2010-12-09 15:31:49 -0800

    fixed the _path bug

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Sampleapp::Application.routes.draw do
 
   root :to => 'pages#home'  
   
-  match '/signup', :to => 'users#new'
+  match '/signin',  :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
   
-  match '/home', :to => 'pages#home'
+  match '/signup',  :to => 'users#new'
+  
+  match '/home',    :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
